ca3675475f7bd0663b657838fb1caa1e4cddad3c
Moritz <47645183+MoClaas@users.noreply.github.com>
2024-10-19 19:46:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const panoramaImage = new PANOLENS.ImagePanorama("../Bilder/Versmold.jpg");
+const imageContainer = document.querySelector(".image-container");
+
+const infospot = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
+infospot.position.set(500, 500, -9000);
+infospot.addHoverText("Bockhorst", 50);
+
+infospot.addEventListener('click', function() {
+    window.open('../Bockhorst/index.html', '_self');
+})
+
+panoramaImage.add(infospot);
+
+const viewer = new PANOLENS.Viewer({
+    container: imageContainer,
+    autoRotate: false,
+    autoRotateSpeed: 0.2,
+    controlBar: false,
+    // zoom: {
+    //     inverted: true
+    // }
+});
+
+viewer.add(panoramaImage);
+
+// viewer.addEventListener('wheel', function(event) {
+//     event.preventDefault(); // Standard-Scrolling verhindern
+    
+//     // Invertiere die Scrollrichtung
+//     if (event.deltaY < 0) {
+//         viewer.zoomOut(); // Scrollen nach oben - hineinzoomen
+//     } else {
+//         viewer.zoomIn(); // Scrollen nach unten - herauszoomen
+//     }
+// });
+
+const menuIcon = document.querySelector('.menu-icon');
+const menuContainer = document.querySelector('.menu-container');
+
+menuIcon.addEventListener('click', () => {
+    menuContainer.classList.toggle('open'); // "open"-Klasse wird hinzugefügt oder entfernt
+});
